20b024acf3a04c1d9fc82ee4c0b3eeac672af388
Sachiko Yamasaki <sachiko6248@gmail.com>
2019-02-14 17:42:18 -0800

    friend-finder app

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'BB-8',
+		photo: 'https://starwarsblog.starwars.com/wp-content/uploads/2016/09/sphero-bb8-38-1536x864-198804688127.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Porg',
+		photo: 'https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2017/10/screen_shot_2017-10-31_at_11.32.11_am_3_-_h_2017.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Stitch',
+		photo: 'https://d.facdn.net/art/adrianasilva/1407616133/1407616133.adrianasilva_stitch.png',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Woody',
+		photo: 'https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/451692',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Buzz',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzp8GOrOawXms-XATj_yQVsWmdmf_D4G3D4j04Owqnx95aqRkJ',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Yeti',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNeMlO1A6leb97YGiAafKQDwCYDm-KY6RHokj_ckM_HepPEepH',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'RBG',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Ruth_Bader_Ginsburg_2016_portrait.jpg/220px-Ruth_Bader_Ginsburg_2016_portrait.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Simba',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/c/cf/YoungSimbaSmilestlk.png',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Jack Jack',
+		photo: 'https://i.pinimg.com/originals/cc/72/58/cc725865116aa1711c9c1762f5faa50a.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
